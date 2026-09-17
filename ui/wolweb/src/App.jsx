ccc83651c1b8fb05fa9129a911e7913c5f19cc28
Lajos Koszti <ajnasz@ajnasz.hu>
2026-09-17 08:25:20 +0200

    Revert "fix(ui): 1 col grid until md"
    This reverts commit 08f9d6dc91538ea195d23e98f1fb3c469ab55aed.

--- ui/wolweb/src/App.jsx
+++ ui/wolweb/src/App.jsx
@@ -298,7 +298,7 @@ function App() {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4">
       {macs.map((mac) => <MacAddressButton
         key={mac.Address}
         mac={mac}
